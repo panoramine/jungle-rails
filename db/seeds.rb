e886a214c3b7807a3f63abd04e83d6fa410581be
panoramine <camila.c.a.salles@gmail.com>
2021-11-11 13:17:37 -0800

    add admin/categories feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,9 +25,9 @@ end
 
 puts "Finding or Creating Categories ..."
 
-cat1 = Category.find_or_create_by! name: 'Apparel'
-cat2 = Category.find_or_create_by! name: 'Electronics'
-cat3 = Category.find_or_create_by! name: 'Furniture'
+cat1 = Category.find_or_create_by! name: 'Apparel', description: "Men's and women`s clothing"
+cat2 = Category.find_or_create_by! name: 'Electronics', description: "Electric devices"
+cat3 = Category.find_or_create_by! name: 'Furniture', description: "Beds, chairs and more!"
 
 ## PRODUCTS
 
