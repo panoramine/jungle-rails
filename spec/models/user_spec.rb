9303e58755007b66d81bc27511f576621f190cb6
panoramine <camila.c.a.salles@gmail.com>
2021-11-16 17:38:50 -0800

    fix user name attribute and add first user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'Validations' do
+
+    describe 'test first_name validation' do
+      it 'expect error if first_name is nil' do
+        @user = User.create(first_name: nil, last_name: "Smith", email: "amysmith@email.com", password: "111", password_confirmation: "111")
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+    end
+
+
+  end
 end
